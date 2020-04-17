b4088dd7be424dbb57e5f0c05936aa53e2c6e8a3
Josh Schoenecker <schoeneckerj@gmail.com>
2020-04-17 13:58:28 -0600

    init

---
+++ src/components/blog.vue
@@ -0,0 +1,25 @@
+<template>
+ <div class="blog">
+  <p>{{blogData.title}}</p>
+  <p>{{blogData.body}}</p>
+ </div>
+</template>
+
+
+<script>
+export default {
+    name: 'blog',
+    props: ["blogData"],
+    data(){
+        return {}
+    },
+    computed:{},
+    methods:{},
+    components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
