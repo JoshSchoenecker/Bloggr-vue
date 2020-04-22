6eee4f44d91776e8793764c387f5cf6914a94555
Josh Schoenecker <schoeneckerj@gmail.com>
2020-04-21 18:17:38 -0600

    Edit blog post done

--- src/pages/BlogDetails.vue
+++ src/pages/BlogDetails.vue
@@ -4,15 +4,29 @@
     <div class="row m-auto">
       <div class="col-10 text-center m-auto" v-if="blogInfo.blog">
         <div class="card card-body mt-5">
-          <!-- TODO delete button. move to left side? -->
-          <div class="row">
+          <!-- TODO delete button. move to right side? -->
+          <div class="row justify-content-end">
         <button class="text-danger" v-if="$auth.userInfo.name == blogInfo.blog.creator.name" @click="deleteItem()"> 
           <span>&times;</span>
         </button>
           </div>
           <h4>{{blogInfo.blog.title}}</h4>
           <h5>By: {{blogInfo.blog.creator.name}}</h5>
-          <p>{{blogInfo.blog.body}}</p>
+          <hr>
+          <h5>{{blogInfo.blog.body}}</h5>
+          <hr>
+          <!-- edit blog button -->
+          <button style="width:3rem" v-if="$auth.userInfo.name == blogInfo.blog.creator.name" @click="editing = !editing">
+            Edit Post
+          </button>
+          
+          <form v-if="editing" @submit.prevent="editItem()">
+            <input type="text" v-model="blogInfo.blog.body">
+            <button v-if="$auth.userInfo.name == blogInfo.blog.creator.name" type="submit">
+              Submit Change
+            </button>
+          </form>
+          
         </div>
       </div>
     </div>
@@ -23,7 +37,9 @@
 export default {
   name: "BlogPage",
   data() {
-    return {};
+    return {
+      editing: false
+    };
   },
   mounted() {
     this.$store.dispatch("getBlog", this.$route.params.blogId);
@@ -37,8 +53,12 @@ export default {
   methods: {
     deleteItem(){
       debugger
-      let deleteBlog = this.$route.params.blogId 
-      this.$store.dispatch("deleteBlog", deleteBlog)
+      let deleteItem = this.$route.params.blogId 
+      this.$store.dispatch("deleteBlog", deleteItem)
+    },
+    editItem(){
+      let editItem = this.blogInfo.blog
+      this.$store.dispatch("editBlog", editItem)
     }
   },
   components: {}
